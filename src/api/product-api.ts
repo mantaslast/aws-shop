b61a3965dcb8c2d21648ae675dd1ece7508c878e
Mantas Lastakauskas <mantaslastakauskas@wixs-MBP.zyxel.com>
2022-05-25 22:59:54 +0300

    Init

---
+++ src/api/product-api.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+import { API_PATHS } from '@/constants/api-paths';
+import { Product } from '@/models/product';
+
+import productList from './productList.json';
+
+const fetchAvailableProducts = async (): Promise<Product[]> => {
+	return axios
+		.get(`${API_PATHS.bff}/product/available/`)
+		.then(res => res.data)
+		.catch(e => {
+			console.error(e);
+			// << !!! mocks if any error !!!
+			return productList;
+		});
+};
+
+const fetchProducts = async (): Promise<Product[]> => {
+	return axios
+		.get(`${API_PATHS.bff}/product`)
+		.then(res => res.data)
+		.catch(e => {
+			console.error(e);
+			// << !!! mocks if any error !!!
+			return productList;
+		});
+};
+
+const fetchProductById = async (id: string) => {
+	console.info(`GET fetchProductById: ${id}`);
+
+	return axios.get(`${API_PATHS.bff}/product/${id}`).then(res => res.data);
+};
+
+const deleteProductById = (id: string) => {
+	console.info(`DELETE deleteProductById: ${id}`);
+
+	return axios.delete(`${API_PATHS.bff}/product/${id}`);
+};
+
+const saveProduct = (productToSave: Product) => {
+	console.info(`PUT saveProduct: ${JSON.stringify(productToSave)}`);
+
+	return axios.put(`${API_PATHS.bff}/product`, productToSave);
+};
+
+export const productApi = {
+	fetchAvailableProducts,
+	deleteProductById,
+	fetchProducts,
+	fetchProductById,
+	saveProduct,
+};
